bb782b7eaa160dda171a7e99cc1f5ab2b18de941
shreekisan <koiralashreekrishna@gmail.com>
2021-10-10 15:11:48 -0500

    first commit

--- src/test/java/testCases/Get_or_read_A_Product.java
+++ src/test/java/testCases/Get_or_read_A_Product.java
@@ -1,5 +1,8 @@
 package testCases;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItems;
+
 import org.testng.annotations.Test;
 
 import io.restassured.response.Response;
@@ -7,12 +10,13 @@ import io.restassured.response.Response;
 import static io.restassured.RestAssured.*;
 public class Get_or_read_A_Product {
 	
-	@Test
+	//@Test
 	public void getData()
 	{ 
 		Response response = get("http://techfios.com/api-prod/api/product/read_one.php/?id=2015");
 		//System.out.println(response.asString());
 		response.prettyPrint();
 	}
-
+	
+	
 }
